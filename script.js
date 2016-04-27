a3828af1115274ca7383d9f88a18415b76a7a59e
Lucy Zhang <lucy.7a11@gmail.com>
2016-04-27 00:27:46 +0000

    Changed title

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ function getPopUp(){
 }
 
 //create buttons
-for (var i=0; i<50; i++){
+for (var i=0; i<55; i++){
 	var btn = document.createElement("button");        // Create a <button> element
-	var t = document.createTextNode("Click me");     // Create a text node
+	var t = document.createTextNode("Click ME!");     // Create a text node
 	btn.className="button";
 	btn.appendChild(t);                                // Append the text to <button>
 	document.body.appendChild(btn);                    // Append <button> to <body>	
